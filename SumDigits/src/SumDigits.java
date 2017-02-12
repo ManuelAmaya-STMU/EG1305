0cc56035ba53d2fa12e12a4d6c91416736e8dfc9
aktunc-STMU <aktunc@gmail.com>
2017-02-12 11:21:12 -0600

    Check comments on formatting

--- SumDigits/src/SumDigits.java
+++ SumDigits/src/SumDigits.java
@@ -1,10 +1,13 @@
+//Use space on both sides of an operator
+//Use Ctrl+Shift+F for auto formatting in Eclipse
+
 import java.util.Scanner;
 public class SumDigits {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a;
-		int sum =0;
+		int sum =0; 
 		System.out.print("Please choose an integer between 0 and 1000\n");
 		Scanner keyboard = new Scanner(System.in);
 		a = keyboard.nextInt();
@@ -15,9 +18,9 @@ public class SumDigits {
 					+ "Please Try Again!!!!\n");
 		}
 		while (a >0){
-			int add = a%10;
+			int add = a%10; //int add = a % 10;
 			sum = sum +add;
-			a=a/10;
+			a=a/10; //a  = a / 10;
 		}
 		System.out.println("The sum of all digits is:" +sum);
 	}
