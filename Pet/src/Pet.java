3817ffa352c47eed6325823ca044bf7d77ff5741
ManuelAmaya-STMU <mamaya1@mail.stmarytx.edu>
2017-03-21 22:54:09 -0500

    Add files via upload

---
+++ Pet/src/Pet.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Pet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	public static String getName() {
+		
+		return null;
+	}
+
+}
